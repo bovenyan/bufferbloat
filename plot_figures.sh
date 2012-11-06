c637aec09716693f260f9019fd6aeaadbf75349c
Te-Yuan Huang <huangty@stanford.edu>
2012-11-06 20:46:23 +0000

    add link

--- plot_figures.sh
+++ plot_figures.sh
@@ -12,9 +12,23 @@ python plot_queue.py --maxy 500 --miny 0 -f ${exp}_sw0-qlen.txt -o ${exp}_queue.
 python plot_tcpprobe.py -f ${exp}_tcpprobe.txt -o ${exp}_tcp_cwnd_iperf.png -p 5001 >/dev/null
 python plot_tcpprobe.py -f ${exp}_tcpprobe.txt -o ${exp}_tcp_cwnd_wget.png -p 80 --sport >/dev/null
 
-echo "Use http://$IP:8888/FIGURE_NAME to see the figures on your browser"
+echo "Use http://$IP:8888/ to see the figures on your browser"
 echo "Figure Names"
 echo "Queue : ${exp}_queue.png"
 echo "IPERF CWND : ${exp}_tcp_cwnd_iperf.png"
 echo "WGET CWND : ${exp}_tcp_cwnd_wget.png"
+
+rm -f index.html
+touch index.html
+echo "<html><head><title>FIGUES</title></head>" > index.html
+echo "<body><cetner><table border=\"1\">" >> index.html
+echo "<tr><th>Switch Queue Occupancy</th></tr>" >> index.html
+echo "<tr><td><a href=\"${exp}_queue.png\"><img src=\"${exp}_queue.png\"/></a></td></tr>" >> index.html
+echo "<tr><th>TCP CWND for iperf</th></tr>" >> index.html
+echo "<tr><td><a href=\"${exp}_tcp_cwnd_iperf.png\"><img src=\"${exp}_tcp_cwnd_iperf.png\"/></a></td></tr>" >> index.html
+echo "<tr><th>TCP CWND for wget</th></tr>" >> index.html
+echo "<tr><td><a href=\"${exp}_tcp_cwnd_wget.png\"><img src=\"${exp}_tcp_cwnd_wget.png\"/></a></td></tr>" >> index.html
+echo "</table></center></body></html>" >> indx.html
+
+#sudo pkill -9 -f SimpleHTTPServer
 python -m SimpleHTTPServer 8888
