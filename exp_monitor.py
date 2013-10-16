6c0abe40b59af0283084fd1ae6ea0b8753c6fc87
Vimalkumar <j.vimal@gmail.com>
2013-10-16 16:42:38 -0700

    Better help msgs.

--- exp_monitor.py
+++ exp_monitor.py
@@ -19,7 +19,7 @@ args = parser.parse_args()
 
 def start_tcpprobe():
     "Install tcp_pobe module and dump to file"
-    os.system("rmmod tcp_probe; modprobe tcp_probe full=1;")
+    os.system("(rmmod tcp_probe >/dev/null 2>&1); modprobe tcp_probe full=1;")
     print "Monitoring TCP CWND ... will save it to ./%s_tcpprobe.txt " % args.exp
     Popen("cat /proc/net/tcpprobe > ./%s_tcpprobe.txt" %
           args.exp, shell=True)
@@ -28,10 +28,11 @@ def qmon():
     monitor = Process(target=monitor_qlen,args=('s0-eth2', 0.01, '%s_sw0-qlen.txt' % args.exp ))
     monitor.start()
     print "Monitoring Queue Occupancy ... will save it to %s_sw0-qlen.txt " % args.exp
-    raw_input('Enter to stop monitor--> ')
+    raw_input('Press Enter key to stop the monitor--> ')
     monitor.terminate()
 
 if __name__ == '__main__':
     start_tcpprobe()
     qmon()
     Popen("killall -9 cat", shell=True).wait()
+
